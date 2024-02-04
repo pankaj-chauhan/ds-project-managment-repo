25c885db52c8a025a7445e8866e154a5a8d9742e
Pankaj Chauhan <pankajchauhan.687@gmail.com>
2024-02-04 20:56:18 +0530

    cookiecutter first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To Learn how to manage the data science projects',
+    author='Pankaj Chauhan',
+    license='',
+)
